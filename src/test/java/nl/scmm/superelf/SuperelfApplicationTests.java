928dab15d37e1208d46c08b7b33fc2407bfea1d3
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2023-05-10 08:55:19 +0200

    added spring boot

---
+++ src/test/java/nl/scmm/superelf/SuperelfApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.scmm.superelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
