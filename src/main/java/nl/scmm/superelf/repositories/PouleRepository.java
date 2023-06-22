089ae7e705002e503bb812ca4560a807d04c369b
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2023-06-22 14:08:37 +0200

    added repositories + getusers in service

---
+++ src/main/java/nl/scmm/superelf/repositories/PouleRepository.java
@@ -0,0 +1,4 @@
+package nl.scmm.superelf.repositories;
+
+public interface PouleRepository {
+}
--- src/main/java/nl/scmm/superelf/services/UserService.java
+++ src/main/java/nl/scmm/superelf/services/UserService.java
@@ -1,6 +1,8 @@
 package nl.scmm.superelf.services;
 
 import nl.scmm.superelf.models.User;
+import nl.scmm.superelf.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,8 +10,15 @@ import java.util.List;
 
 @Service
 public class UserService {
-    public static List<User> getUsers() {
-        List<User> users = new ArrayList<>();
-        return users;
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
